e9bff7cc4abebd9cff7233f4bb1117713a921a79
gabriel <metalmadnes98@gmail.com>
2022-04-19 18:28:57 -0500

    Se arego una nueva prueba la cual provoca un error

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,9 +1,19 @@
+const MissionCommander = require('./../app/missionCommander');
 // prueba basica proporcionada por la documentacion de jest
+describe("Unit Tests for Mission Commander Class", () => {
+        test('1) Create a mission commander objet', () => {
+            // Se realiza el codigo como queremos que se use
+            const myMissionCommander = new MissionCommander("Woopa")
+                // Validacion de resultados esperados
+            expect(myMissionCommander.name).toBe("Woopa");
+        });
+    })
+    // prueba con error
 describe("Unit Tests for Mission Commander Class", () => {
     test('1) Create a mission commander objet', () => {
         // Se realiza el codigo como queremos que se use
-        const myMissionCommander = new MissionCommander("Woopa")
+        const myMissionCommander2 = new MissionCommander("Woopa")
             // Validacion de resultados esperados
-        expect(myMissionCommander.name).toBe("Woopa");
+        expect(myMissionCommander2.name).toBe("Wooper");
     });
 })
